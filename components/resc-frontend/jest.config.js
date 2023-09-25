0c9219d6219d66075aab7f74c8847437d06db72c
Usman2ABN <113449186+Usman2ABN@users.noreply.github.com>
2023-09-25 13:48:18 +0530

    Update jest.config.js

--- components/resc-frontend/jest.config.js
+++ components/resc-frontend/jest.config.js
@@ -4,7 +4,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./jest.setup.js'],
   collectCoverage: true,
-  testResultsProcessor: 'jest-sonar-reporter',
   coverageDirectory: 'tests/unit/reports/coverage',
   coverageReporters: ['lcov', 'text', 'cobertura'],
   collectCoverageFrom: [
